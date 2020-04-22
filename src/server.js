8495647fca1566d4b15a61c89afe3dde86998257
Roman Vaivod <romanv@salsitasoft.com>
2020-04-22 20:06:48 +0200

    Add server

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const PORT = 3001;
+const todos = [];
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get("/todos", (req, res) => res.json(todos));
+
+app.post("/todos", (req, res) => {
+    const { name } = req.body;
+    const user = {
+        id: todos.length + 1,
+        name,
+    };
+
+    todos.push(user);
+    res.json(user);
+});
+
+app.listen(PORT, () => console.log(`The server is running on port ${PORT}!`));
